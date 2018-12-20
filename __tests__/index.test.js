d4b2741b0220857b3aec27f73c61df28d7ce2642
Matthew Chan <matthewchan@Matthews-MacBook-Pro.local>
2018-12-20 01:21:17 -0500

    Added methods for retrieving items

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -13,3 +13,15 @@ test("fetches champions data from the LoL static data cdn", async () => {
   const champions = await lol.getChampions();
   return expect(champions.length).toBe(2);
 });
+
+test("fetches Boots of Speed data from the LoL static data cdn", async () => {
+  const item = await lol.getItem({
+    item: "Boots of Speed"
+  });
+  return expect(item.name).toEqual("Boots of Speed");
+});
+
+test("fetches items data from the LoL static data cdn", async () => {
+  const items = await lol.getItems();
+  return expect(items.length).toBe(2);
+});
